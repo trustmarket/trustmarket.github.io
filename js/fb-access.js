547de1f8660a1cb1c20dcb6bbc24afcbdc576870
rubykv <rubyshiv@gmail.com>
2017-04-03 15:23:09 -0400

    testing

--- js/fb-access.js
+++ js/fb-access.js
@@ -23,3 +23,17 @@ window.fbAsyncInit = function() {
    js.src = "https://connect.facebook.net/en_US/sdk.js";
    fjs.parentNode.insertBefore(js, fjs);
  }(document, 'script', 'facebook-jssdk'));
+
+ function welcomeMsg() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      alert("Yes!!!");
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+  }
+  function loginFailedMsg() {
+    document.getElementById('status').innerHTML =
+    'Please try login again later ' + '!';
+   }
