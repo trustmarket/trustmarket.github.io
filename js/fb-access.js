91afe34990568109d1c31dd7de296a20845bfb02
rubykv <rubyshiv@gmail.com>
2017-04-04 09:16:54 -0400

    removed user-friends logic

--- js/fb-access.js
+++ js/fb-access.js
@@ -38,9 +38,9 @@ function statusChangeCallback(response) {
 
 function testAPI() {
   console.log('Welcome!  Fetching your information.... ');
-  FB.api('/me',{fields: 'user_friends'}, function(response) {
+  FB.api('/me',function(response) {
     console.log('Successful login for: ' + response.name);
     document.getElementById('status').innerHTML =
-      'Thanks for logging in, ' + response.name + '!'+response.user_friends;
+      'Thanks for logging in, ' + response.name + '!';
   });
 }
